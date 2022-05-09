9eb2f3c4ec9369a7531c6b0da6e8b21ea74e7271
github-action-benchmark <github@users.noreply.github.com>
2022-05-09 09:56:15 +0000

    add Card orchestrator benchmark (customSmallerIsBetter) benchmark result for 771645ecbbacc990dc339a56b70c25e080fe04d8

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1652090175619,
+  "repoUrl": "https://github.com/madhan-mettle/jmh-benchmark-test",
+  "entries": {
+    "Card orchestrator benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "85488123+madhan-mettle@users.noreply.github.com",
+            "name": "Madhanraj Maraimani",
+            "username": "madhan-mettle"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "771645ecbbacc990dc339a56b70c25e080fe04d8",
+          "message": "Merge pull request #3 from madhan-mettle/temp1\n\ncheck push",
+          "timestamp": "2022-05-09T10:55:20+01:00",
+          "tree_id": "98f8eb74d95f74af95612eea4311ec289cddcfad",
+          "url": "https://github.com/madhan-mettle/jmh-benchmark-test/commit/771645ecbbacc990dc339a56b70c25e080fe04d8"
+        },
+        "date": 1652090174758,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "benchmark.BenchmarkTest.test1",
+            "value": 3.0683833090909087,
+            "unit": "ms/op",
+            "extra": "iterations: 3\nforks: 5\nthreads: 1\nmode: avgt"
+          },
+          {
+            "name": "benchmark.BenchmarkTest.test2",
+            "value": 3.0676140244206778,
+            "unit": "ms/op",
+            "extra": "iterations: 3\nforks: 5\nthreads: 1\nmode: avgt"
+          }
+        ]
+      }
+    ]
+  }
+}
